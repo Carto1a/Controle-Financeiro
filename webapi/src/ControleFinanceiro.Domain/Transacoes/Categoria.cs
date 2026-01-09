dc22940510c63cde3cd5a35a7ba490678daa05af
Cartola <49589600+Carto1a@users.noreply.github.com>
2026-01-09 01:02:57 -0300

    feat(Categoria): nome

--- webapi/src/ControleFinanceiro.Domain/Transacoes/Categoria.cs
+++ webapi/src/ControleFinanceiro.Domain/Transacoes/Categoria.cs
@@ -5,23 +5,28 @@ namespace ControleFinanceiro.Domain.Transacoes;
 public class Categoria
 {
     public Guid Id { get; private set; }
+    public string Nome { get; private set; }
     public string Descricao { get; private set; }
     public Finalidade Finalidade { get; private set; }
 
     private Categoria() { }
-    private Categoria(Guid id, string descricao, Finalidade finalidade)
+    private Categoria(Guid id, string nome, string descricao, Finalidade finalidade)
     {
         Id = id;
+        Nome = nome;
         Descricao = descricao;
         Finalidade = finalidade;
     }
 
-    public static Result<Categoria> Criar(string descricao, Finalidade finalidade)
+    public static Result<Categoria> Criar(string nome, string descricao, Finalidade finalidade)
     {
         if (string.IsNullOrWhiteSpace(descricao))
             return Result.Fail("O descricao é obrigatório e não pode ser vazio ou conter apenas espaços em branco");
 
-        var categoria = new Categoria(Guid.NewGuid(), descricao, finalidade);
+        if (string.IsNullOrWhiteSpace(nome))
+            return Result.Fail("O nome é obrigatório e não pode ser vazio ou conter apenas espaços em branco");
+
+        var categoria = new Categoria(Guid.NewGuid(), nome, descricao, finalidade);
 
         return categoria;
     }
