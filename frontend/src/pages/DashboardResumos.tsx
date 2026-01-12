c506e33f04eca5240d98788bacc2f4f0bb11eac1
Cartola <49589600+Carto1a@users.noreply.github.com>
2026-01-12 15:10:20 -0300

    feat(frontend/resumos): tabela basica

--- frontend/src/pages/DashboardResumos.tsx
+++ frontend/src/pages/DashboardResumos.tsx
@@ -1,5 +1,84 @@
+import type { Paginated } from "@/api/axios";
+import DataTable from "@/components/data-table";
+import { resumosService, type ResumoFinanceiro } from "@/services/resumos";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+  type ColumnDef,
+  type PaginationState
+} from "@tanstack/react-table";
+import { useMemo, useState } from "react";
+
 export default function DashboardResumos() {
-  return(
-    <span>DashboardResumos</span>
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  })
+
+  const columns: ColumnDef<ResumoFinanceiro>[] = [
+    {
+      header: "Nome",
+      accessorKey: "nome"
+    },
+    {
+      header: "Total Receitas",
+      accessorKey: "totalReceitas"
+    },
+    {
+      header: "Total Despesas",
+      accessorKey: "totalDespesas"
+    },
+    {
+      header: "Saldo",
+      accessorKey: "saldo",
+      cell: info => {
+        const value = info.getValue() as number;
+
+        const colorClass = value > 0
+          ? "text-green-600"
+          : value < 0
+            ? "text-red-600"
+            : "text-gray-500";
+
+        return (
+          <span className={colorClass}>
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL"
+            }).format(value)}
+          </span>
+        );
+      }
+    }
+  ]
+
+  const defaultData = useMemo(() => [], [])
+
+  const dataQuery = useQuery<Paginated<ResumoFinanceiro>, unknown>({
+    queryKey: ['data', pagination],
+    queryFn: () => resumosService.porCategoria({ pagina: pagination.pageIndex, tamanhoPagina: pagination.pageSize }),
+    placeholderData: keepPreviousData,
+  })
+
+  const table = useReactTable({
+    data: dataQuery.data?.items ?? defaultData,
+    columns,
+    rowCount: dataQuery.data?.total ?? 0,
+    manualSorting: true,
+    manualPagination: true,
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      pagination
+    },
+  })
+
+  return (
+    <>
+      <DataTable table={table} dataQuery={dataQuery} />
+    </>
   )
 }
--- frontend/src/services/resumos.ts
+++ frontend/src/services/resumos.ts
@@ -1,4 +1,4 @@
-import { api, type PaginatedQuery } from "../api/axios";
+import { api, type Paginated, type PaginatedQuery } from "../api/axios";
 
 export interface ResumoFinanceiroTotal {
   totalReceitas: number;
@@ -15,10 +15,10 @@ export const resumosService = {
   totalFinanceiro: async (query: PaginatedQuery, signal: AbortSignal | undefined = undefined): Promise<ResumoFinanceiroTotal> =>
     (await api.get('/resumos/financeiro/total', { params: query, signal: signal })).data,
 
-  porPessoa: async (query: PaginatedQuery, signal: AbortSignal | undefined = undefined): Promise<ResumoFinanceiro[]> =>
+  porPessoa: async (query: PaginatedQuery, signal: AbortSignal | undefined = undefined): Promise<Paginated<ResumoFinanceiro>> =>
     (await api.get('/resumos/financeiro/pessoas', { params: query, signal: signal })).data,
 
-  porCategoria: async (query: PaginatedQuery, signal: AbortSignal | undefined = undefined): Promise<ResumoFinanceiro[]> =>
+  porCategoria: async (query: PaginatedQuery, signal: AbortSignal | undefined = undefined): Promise<Paginated<ResumoFinanceiro>> =>
     (await api.get('/resumos/financeiro/categorias', { params: query, signal: signal })).data,
 };
 
